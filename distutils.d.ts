335cbe87a33c4b6981e53011a9bb67cd8099f8d9
Anthony Fu <anthonyfu117@hotmail.com>
2021-01-06 05:51:37 +0800

    chore: clean up

--- distutils.d.ts
+++
@@ -1,6 +0,0 @@
-import { Reactify } from '@vueuse/shared';
-declare type PrependArgument<Fn, E> = Fn extends (...args: infer R) => infer T ? (that: E, ...args: R) => T : never;
-export declare function reactifyMethod<T extends Function>(fn: T): Reactify<PrependArgument<T, any>>;
-export declare const reactifyString: <T extends Function>(fn: T) => Reactify<PrependArgument<T, string>>;
-export declare const reactifyNumber: <T extends Function>(fn: T) => Reactify<PrependArgument<T, number>>;
-export {};
