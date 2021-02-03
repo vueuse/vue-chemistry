1319371b740e3e43eb8d778bcbf4b3af16f1484d
Anthony Fu <anthonyfu117@hotmail.com>
2021-02-04 04:55:47 +0800

    fix: reactify get

--- src/core.ts
+++ src/core.ts
@@ -1,1 +1,6 @@
-export { reactify, reactifyObject, get, set, when, MaybeRef } from '@vueuse/shared'
+import { reactify, get as _get } from '@vueuse/shared'
+
+export { reactify, reactifyObject, set, when, MaybeRef } from '@vueuse/shared'
+
+/*@__PURE__*/
+export const get = reactify(_get)
